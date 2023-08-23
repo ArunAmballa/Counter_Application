6545238cb59bd18a29809b35a168d9515c4a2b02
arun <arunamballa24@gmail.com>
2023-08-22 19:10:14 -0700

    Added JS

---
+++ index.js
@@ -0,0 +1,12 @@
+let count=document.querySelector('#counter')
+function decrement(){
+    let value=parseInt(count.innerText);
+    value=value-1
+    count.innerText=value
+
+}
+function increment(){
+    let value=parseInt(count.innerText);
+    value=value+1
+    count.innerText=value
+}
